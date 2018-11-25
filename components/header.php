4b1f1c75cbce0793a5d769727bfa6562c9929fde
Peter Tiernan <ptiernan@gmail.com>
2018-11-25 14:54:35 +0000

    first commit

---
+++ components/header.php
@@ -0,0 +1,23 @@
+<div class="main-nav-logo-left">
+	<div class="logo">
+		<h3>0044 123456777</h3>
+		<img src="img/white-logo.png" alt="">
+
+
+	</div>
+	<div class="nav">
+		<li>
+			<a href="#!">Home</a>
+		</li>
+		<li>
+			<a href="#!">Projects</a>
+		</li>
+		<li>
+			<a href="#!">Contact</a>
+		</li>
+		<li>
+			<a href="#!">News</a>
+		</li>
+	</div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include 'components/head.php';?>
+	<title>CSI Home - Experts in ... </title>
+</head>
+
+<body>
+	<div class="container">
+		<?php include 'components/header.php'; ?>
+
+		<div class="banner">
+			<div></div>
+			<div class="banner-content">
+				<h1>Lorem, ipsum dolor sit amet consectetur adipisicing.</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum laudantium quos dolore voluptatibus cupiditate! Illum repellat
+					libero voluptas molestias consectetur?
+				</p>
+				<a href="#!" class="btn-flat">Read More</a>
+			</div>
+		</div>
+
+		<div class="rowfull">
+			<h1>About us</h1>
+		</div>
+
+		<div class="about-us-background">
+
+		</div>
+
+		<div class="about-us">
+			<div class="about">
+				<h2>Expertise</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate beatae aut aliquid accusamus deleniti. Impedit eligendi
+					iure velit quas optio sequi. Ad quidem, fugiat veniam ea nesciunt aliquam aperiam itaque.</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsam, ducimus, tempora atque vero facere eveniet quis
+					consequatur sit dignissimos iusto temporibus, fuga tenetur architecto.</p>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit architecto veritatis dolores optio, saepe dicta officia
+					debitis blanditiis nam voluptatum doloremque magnam odit at animi eos! Nobis, ipsa dignissimos. Vitae blanditiis fugiat
+					minus recusandae molestiae.</p>
+			</div>
+			<div class="special">
+				<h2>We specialise in</h2>
+				<div class="sp1">
+					<i class="fas fa-torii-gate"></i>
+					<h4>Commercial Fit Out</h4>
+				</div>
+				<div class="sp2">
+					<i class="fas fa-torii-gate"></i>
+					<h4>Commercial Fit Out</h4>
+				</div>
+				<div class="sp3">
+					<i class="fas fa-torii-gate"></i>
+					<h4>Commercial Fit Out</h4>
+				</div>
+				<div class="sp4">
+					<i class="fas fa-torii-gate"></i>
+					<h4>Commercial Fit Out</h4>
+				</div>
+			</div>
+			<img src="img/banner.jpg" alt="">
+		</div>
+
+		<div class="projects-banner">
+			<h1>Our Projects</h1>
+		</div>
+
+
+
+	</div>
+
+</body>
+
+</html>
