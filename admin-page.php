f3e16fc99541930010c3cf0a88fda7a9e0413e75
Peter Tiernan <ptiernan@gmail.com>
2018-11-28 23:25:25 +0000

    added login page

--- admin-page.php
+++ admin-page.php
@@ -1,3 +1,6 @@
+<?php 
+require 'components/init.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +12,35 @@
 <body>
 	<div class="container">
 		<div class="admin-banner">
+			<h1>CSI Admin Panel</h1>
 
+		</div>
+
+		<div class="admin-content-background">
+		</div>
+		<div class="admin-content">
+			<?php if (Auth::isLoggedIn()): ?>
+			<div class="admin-login">
+				<p>You are logged in.
+					<a href="logout.php">Log out</a>
+				</p>
+
+
+
+
+
+
+
+
+
+
+				<?php else: ?>
+				<p class="not-logged-in">Your are not logged in... Please
+					<a href="login.php">Log In</a>
+
+				</p>
+				<?php endif; ?>
+			</div>
 		</div>
 
 		<?php include 'components/footer.php';?>
---
+++ login.php
@@ -0,0 +1,78 @@
+<?php
+require 'components/init.php';
+
+// $_SESSION['is_logged_in'] = true;
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+	
+	$conn = require 'components/db.php';
+
+    if (User::authenticate($conn, $_POST['username'], $_POST['password'])) {
+        Auth::login();
+        Url::redirect("/drumcondrafc/admin-page.php");
+    } else {
+        $error = 'login incorrect';
+    }
+}
+
+?>
+
+<html lang="en">
+
+<head>
+	<?php include 'components/head.php';
+    ?>
+	<title>Admin Panel Log In</title>
+</head>
+
+<body>
+
+	<div class="admin-container">
+
+
+
+		<div class="admin-banner">
+			<h1>CSI Admin Panel </h1>
+		</div>
+
+		<div class="form-container">
+			<form action="login.php" method="POST" class="contact-form login-form">
+				<?php if (! empty($error)) :?>
+				<p>
+					<?= $error ;?>
+				</p>
+				<?php endif; ?>
+				<div class="form-group">
+					<label for="username">Username:</label>
+					<input type="text" name="username" id="username">
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" id="password" name="password">
+				</div>
+				<button>Log in</button>
+			</form>
+			<a href="admin-page.php">Go Back</a>
+		</div>
+
+
+
+		<?php include 'components/footer.php'; 
+        ?>
+
+	</div>
+
+
+	<script src="js/navbar.js"></script>
+	<script src="js/animation.js"></script>
+	<script src="js/banner.js"></script>
+	<script>
+		AOS.init();
+	</script>
+	<script>
+	</script>
+
+</body>
+
+</html>
